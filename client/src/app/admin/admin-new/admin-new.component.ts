3790ef78cd91dae68c1df53edc1438b19b1976bb
Briggitte Martinez <briggitte90@hotmail.com>
2025-05-06 14:26:22 +0200

    new components for admin

---
+++ client/src/app/admin/admin-new/admin-new.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-new',
+  imports: [],
+  templateUrl: './admin-new.component.html',
+  styleUrl: './admin-new.component.scss'
+})
+export class AdminNewComponent {
+
+}
